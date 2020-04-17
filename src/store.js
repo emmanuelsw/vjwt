73005f00c3c6b6702d547077063dbad280192a8c
EmmanuelSW <edhgt89@hotmail.com>
2020-04-16 21:41:59 -0500

    Register users

--- src/store.js
+++ src/store.js
@@ -1,10 +1,26 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+  state: {
+    user: null,
+  },
+  mutations: {
+    SET_USER_DATA(state, user) {
+      state.user = user
+      localStorage.setItem('user', JSON.stringify(user))
+      axios.defaults.headers.common['Authorization'] = `Bearer ${user.token}`
+    }
+  },
+  actions: {
+    register({ commit }, credentials) {
+      return axios.post('http://localhost:3000/register', credentials)
+        .then(({ data }) => {
+          commit('SET_USER_DATA', data)
+        })
+    }
+  }
 })
